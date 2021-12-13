6e121bd3b5fdf7eaede1c7fcb16fd10f03722fa1
Robin Wieschendorf <mail@robinwieschendorf.de>
2021-12-13 13:38:45 +0100

    chore: improve language files

--- new_files/lang/english/modules/system/fw_pdf_documents.php
+++ new_files/lang/english/modules/system/fw_pdf_documents.php
@@ -1,6 +1,10 @@
 <?php
 
-define('MODULE_FW_PDF_DOCUMENTS_TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
-define('MODULE_FW_PDF_DOCUMENTS_LONG_DESCRIPTION', 'Long description for firstweb/pdf-documents');
-define('MODULE_FW_PDF_DOCUMENTS_STATUS_TITLE', 'firstweb/pdf-documents Modul active?');
-define('MODULE_FW_PDF_DOCUMENTS_STATUS_DESC', '');
+$moduleType = 'MODULE';
+$moduleName = 'FW_PDF_DOCUMENTS';
+$prefix = $moduleType . '_' . $moduleName  . '_';
+
+define($pefix . 'TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
+define($pefix . 'LONG_DESCRIPTION', 'Long description for firstweb/pdf-documents');
+define($pefix . 'STATUS_TITLE', 'firstweb/pdf-documents Modul active?');
+define($pefix . 'STATUS_DESC', '');
--- new_files/lang/german/modules/system/fw_pdf_documents.php
+++ new_files/lang/german/modules/system/fw_pdf_documents.php
@@ -1,6 +1,10 @@
 <?php
 
-define('MODULE_FW_PDF_DOCUMENTS_TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
-define('MODULE_FW_PDF_DOCUMENTS_LONG_DESCRIPTION', 'Lange Beschreibung für firstweb/pdf-documents');
-define('MODULE_FW_PDF_DOCUMENTS_STATUS_TITLE', 'firstweb/pdf-documents Modul aktivieren?');
-define('MODULE_FW_PDF_DOCUMENTS_STATUS_DESC', '');
+$moduleType = 'MODULE';
+$moduleName = 'FW_PDF_DOCUMENTS';
+$prefix = $moduleType . '_' . $moduleName  . '_';
+
+define($pefix . 'TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
+define($pefix . 'LONG_DESCRIPTION', 'Lange Beschreibung für firstweb/pdf-documents');
+define($pefix . 'STATUS_TITLE', 'firstweb/pdf-documents Modul aktivieren?');
+define($pefix . 'STATUS_DESC', '');
