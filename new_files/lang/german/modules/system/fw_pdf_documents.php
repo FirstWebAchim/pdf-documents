dd1e03c769c1ba9706a75f78bcb418c24fc56316
Robin Wieschendorf <mail@robinwieschendorf.de>
2022-05-05 14:41:53 +0200

    fix: wrong var name

--- new_files/lang/german/modules/system/fw_pdf_documents.php
+++ new_files/lang/german/modules/system/fw_pdf_documents.php
@@ -4,7 +4,7 @@ $moduleType = 'MODULE';
 $moduleName = 'FW_PDF_DOCUMENTS';
 $prefix = $moduleType . '_' . $moduleName  . '_';
 
-define($pefix . 'TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
-define($pefix . 'LONG_DESCRIPTION', 'Lange Beschreibung für firstweb/pdf-documents');
-define($pefix . 'STATUS_TITLE', 'firstweb/pdf-documents Modul aktivieren?');
-define($pefix . 'STATUS_DESC', '');
+define($prefix . 'TITLE', 'firstweb/pdf-documents © by <a href="#" target="_blank" style="color: #e67e22; font-weight: bold;">firstweb</a>');
+define($prefix . 'LONG_DESCRIPTION', 'Lange Beschreibung für firstweb/pdf-documents');
+define($prefix . 'STATUS_TITLE', 'firstweb/pdf-documents Modul aktivieren?');
+define($prefix . 'STATUS_DESC', '');
